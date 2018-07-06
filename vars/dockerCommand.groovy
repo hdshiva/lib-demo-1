e2ac0aaab0375d214d935d4402a90008286ce8a3
shivamurthy Durugapp <shivamurthy.durugapp@oracle.com>
2018-07-05 20:44:58 -0700

    command update

--- vars/dockerCommand.groovy
+++ vars/dockerCommand.groovy
@@ -1,5 +1,6 @@
 def call(args) {
       assert args != null
       echo "inside dockerCommand(args) method"
+      sh 'docker ${args}'
       return sh(script: "docker ${args}", returnStdout: true)
   }
