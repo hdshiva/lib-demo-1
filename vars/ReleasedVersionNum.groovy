4c64f792f59498a9caabf102bba99c184a7a51ca
shivamurthy Durugapp <shivamurthy.durugapp@oracle.com>
2018-07-05 20:39:59 -0700

    changes

--- vars/ReleasedVersionNum.groovy
+++ vars/ReleasedVersionNum.groovy
@@ -1,3 +1,4 @@
 def call() {
+    echo "Inside the ReleasedVersionNum()"
     (readFile('pom.xml') =~ '<version>(.+)-SNAPSHOT</version>')[0][1]
 }
--- vars/dockerCommand.groovy
+++ vars/dockerCommand.groovy
@@ -1,4 +1,5 @@
 def call(args) {
       assert args != null
+      echo "inside dockerCommand(args) method"
       return sh(script: "docker ${args}", returnStdout: true)
   }
